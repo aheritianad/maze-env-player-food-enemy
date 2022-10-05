b474fde6f3d43aed7e40fe7aa2b65e41278e6276
Heritiana Daniel Andriasolofo <aheritianad@gmail.com>
2022-10-05 13:06:51 +0000

    Change format

--- modules/environment.py
+++ modules/environment.py
@@ -1,32 +1,34 @@
 import numpy as np
 from typing import *
 
+
 class Environment:
     def __init__(self, env_size) -> None:
         self.env_size = env_size
         self.reset()
-    
+
     def reset(self):
-        self.food = np.random.randint(self.env_size, size=2) 
+        self.food = np.random.randint(self.env_size, size=2)
         self.enemy = np.random.randint(self.env_size, size=2)
         while True:
-            self.player = np.random.randint(self.env_size, size=2) # need to fix
+            self.player = np.random.randint(
+                self.env_size, size=2)  # need to fix
             self.get_reward_and_done()
-            if not self._done: # self._done == False  
+            if not self._done:  # self._done == False
                 break
-        
+
         return self.state
-    
+
     @property
     def state(self):
         return "-".join(
-                        list(
-                            map(
-                                lambda xy:str(xy[0])+"_"+str(xy[1]),
-                                [self.player,self.food,self.enemy]
-                                )
-                            )
-                        )
+            list(
+                map(
+                    lambda xy: str(xy[0])+"_"+str(xy[1]),
+                    [self.player, self.food, self.enemy]
+                )
+            )
+        )
 
     def render(self):
         grid = np.zeros((self.env_size, self.env_size), dtype=int)
@@ -47,12 +49,12 @@ class Environment:
             grid[e] = 6
         if e == f and f == p:
             grid[e] = 7
-        
+
         return grid
-    
+
     def __repr__(self):
         grid = self.render()
-        repr = {0:" ", 1:"P", 2:"F", 3:"E", 4:"?", 5:"@", 6:"X", 7:"#"}
+        repr = {0: " ", 1: "P", 2: "F", 3: "E", 4: "?", 5: "@", 6: "X", 7: "#"}
         result = " " + "-"*self.env_size + "\n"
         for row in grid:
             row_string = "|"
@@ -60,20 +62,20 @@ class Environment:
                 row_string += repr[col]
             row_string += "|\n"
             result += row_string
-        result +=  " " + "-"*self.env_size
+        result += " " + "-"*self.env_size
         return result
-    
+
     @property
     def done(self):
         return self._done
-            
+
     def step(self, palyer_direction):
         # move player
-        self.move("player",palyer_direction)
+        self.move("player", palyer_direction)
 
         # always move enemy randomly
-        enemy_direction = np.random.randint(1,5)
-        self.move("enemy",enemy_direction)
+        enemy_direction = np.random.randint(1, 5)
+        self.move("enemy", enemy_direction)
 
         # get reward after a step
         reward, done = self.get_reward_and_done()
@@ -81,20 +83,24 @@ class Environment:
         return reward, next_state, done
 
     def move(self, entity_name, direction):
-        entities = {"player":self.player, "enemy":self.enemy}
+        entities = {"player": self.player, "enemy": self.enemy}
         entity = entities[entity_name]
 
-        if direction == 0: # do not move
+        if direction == 0:  # do not move
             pass
-        elif direction == 1: # left
-            entity[1] = max(0, entity[1] - 1 ) # not allowed to go out of the wall
-        elif direction == 2: # right
-            entity[1] = min(self.env_size - 1, entity[1] + 1 ) # not allowed to go out of the wall
-        elif direction == 3: # up
-            entity[0] = max(0, entity[0] - 1 ) # not allowed to go out of the wall
-        elif direction == 4: # down
-            entity[0] = min(self.env_size - 1, entity[0] + 1 ) # not allowed to go out of the wall
-    
+        elif direction == 1:  # left
+            # not allowed to go out of the wall
+            entity[1] = max(0, entity[1] - 1)
+        elif direction == 2:  # right
+            # not allowed to go out of the wall
+            entity[1] = min(self.env_size - 1, entity[1] + 1)
+        elif direction == 3:  # up
+            # not allowed to go out of the wall
+            entity[0] = max(0, entity[0] - 1)
+        elif direction == 4:  # down
+            # not allowed to go out of the wall
+            entity[0] = min(self.env_size - 1, entity[0] + 1)
+
     def get_reward_and_done(self):
         reward = 0
         done = False
